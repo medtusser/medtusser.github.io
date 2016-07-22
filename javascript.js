0810331f87f1233872c0b304489a479c102b03bf
medtusser <Jens.Hansen@laerdal.com>
2016-07-22 15:07:27 +0200

    2

--- javascript.js
+++
@@ -1,6 +0,0 @@
-var myHostingUrl = "file:///Applications/AMPPS/www/medtusser.github.io/index.html";
-var thisPlace = window.location.href;
-var userPlace = thisPlace.replace(myHostingUrl, "")
-if (userPlace==""){
-	var div = document.getElementById('topArticle');
-}
